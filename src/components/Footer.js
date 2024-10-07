419223a4960761cf38a595716a42743677b05496
Rohit More <cserohitmore@gmail.com>
2024-10-08 01:33:37 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-5">
+      <p className="text-center">
+        All rights reserved &copy; 2024 | Rohit More.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
